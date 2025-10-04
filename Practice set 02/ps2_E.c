96c96cb95f9b973e03bef8a29259c77544a449e9
shhruti147 <154989380+shhruti147@users.noreply.github.com>
2025-10-04 22:32:44 +0530

    Add files via upload

---
+++ Practice set 02/ps2_E.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  /*  3.0 + 1 will be:
+a. Integer.
+b. Floating point number.
+c. Character.
+*/
+float a = 3.0;
+int b = 1;
+float ab = 3.0+1;
+printf("Addition of a float and an integer is always a float\n");
+printf("The value we get by adding %f+%d will be %f \n",a,b,ab);
+    return 0;
+}
