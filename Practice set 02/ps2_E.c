e7e588549cc9eb5f209bcaf769dee54431f3786c
shhruti147 <154989380+shhruti147@users.noreply.github.com>
2025-10-05 23:55:03 +0530

    Update ps2_E.c

--- Practice set 02/ps2_E.c
+++ Practice set 02/ps2_E.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-  /*  3.0 + 1 will be:
+  /* 
+  Problem 5
+  3.0 + 1 will be:
 a. Integer.
 b. Floating point number.
 c. Character.
@@ -13,4 +15,5 @@ float ab = 3.0+1;
 printf("Addition of a float and an integer is always a float\n");
 printf("The value we get by adding %f+%d will be %f \n",a,b,ab);
     return 0;
+
 }
