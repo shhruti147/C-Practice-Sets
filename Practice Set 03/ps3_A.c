a4bc5aac44b9043afff7f81e71edf94ab91378ef
shhruti147 <154989380+shhruti147@users.noreply.github.com>
2025-10-09 23:25:27 +0530

    Add files via upload

---
+++ Practice Set 03/ps3_A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   /* 
+       Problem 1
+       What will be the output of this program
+       int a = 10; if (a = 11) printf("I am 11");
+       else printf("I am not 11");
+   */
+
+   int a = 10;
+   if (a = 11)
+   printf("I am 11");
+   else printf("I am not 11");
+    return 0;
+}
